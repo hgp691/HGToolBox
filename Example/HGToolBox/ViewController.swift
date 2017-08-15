63865170980da6ce3df6993bd2fa9477864d6f70
Alexander Ortiz <alex@pidefarma.com>
2017-08-15 18:13:54 -0500

    Initial commit

---
+++ Example/HGToolBox/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HGToolBox
+//
+//  Created by Alexander Ortiz on 08/15/2017.
+//  Copyright (c) 2017 Alexander Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
